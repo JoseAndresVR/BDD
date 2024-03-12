b0d771c7378f8d25ae758efaf59ccf33582868ce
Jose <jv295222@gmail.com>
2024-03-12 14:18:44 -0500

    Reto 6 H

---
+++ Scripts/deleteRegistros.sql
@@ -0,0 +1,2 @@
+select * from registros_entrada;
+delete from registros_entrada where fecha between '2024/06/01' and '2024/06/30'
---
+++ Scripts/deleteTransacciones.sql
@@ -0,0 +1,2 @@
+select * from transacciones;
+delete from transacciones where hora between '14:00' and '18:00' and fecha between '2024/08/01' and '2024/08/30'
